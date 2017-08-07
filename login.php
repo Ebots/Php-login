dbcb66ad6d8c40f68a3822d3f509ed8f688380fe
Ebots <msawon94@gmail.com>
2017-08-07 06:58:20 +0600

    Update login.php

--- login.php
+++ login.php
@@ -81,8 +81,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
+         body{ font: 14px sans-serif;  }
+        .wrapper{ width: 350px; padding: 20px; margin: auto ;}
     </style>
 </head>
 <body>
